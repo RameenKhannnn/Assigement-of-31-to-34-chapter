3dcec26447c26cac9804e90e221808fe9322f84e
RameenKhannnn <rameenkhan8787@gmail.com>
2024-07-17 23:31:57 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// var monthNames = ["jan","feb","march","april","may","jun","july","aug"]
+// var currentMonth= monthNames[now.getMonth()]
+
+
+ 
+
+// var today = now.getDate()
+// if(today === 1 || today === 21 || today ===31){
+//     today+= 'st'
+// }else if(today ===2 || today == 22 ){
+//     today += 'nd'
+// }else if(today === 3 || today ===23){
+//     today +='rd'
+// }else{
+//     today+= 'th'
+// }
+// var year = now.getFullYear()
+// var currentDay = currentMonth +" " + today+" " + year
+// console.log(currentDay);
+// var getMilisec = now.getTime()
+// console.log(getMilisec);
